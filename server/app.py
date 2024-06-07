9a5ec622562a110aff1fddab46c39102f7c1c895
Jospine <nzi19001@byui.edu>
2024-06-07 11:55:28 +0300

    not  working

--- server/app.py
+++ server/app.py
@@ -1,25 +1,27 @@
-# server/app.py
-
-from flask import Flask
-from flask_migrate import Migrate
-
-from models import db
-
-# create a Flask application instance 
-app = Flask(__name__)
-
-# configure the database connection to the local file app.db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# configure flag to disable modification tracking and use less memory
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# create a Migrate object to manage schema modifications
-migrate = Migrate(app, db)
-
-# initialize the Flask application to use the database
-db.init_app(app)
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+from flask_sqlalchemy import SQLAlchemy
+
+
+from sqlalchemy import MetaData
+
+# contains definitions of tables and associated schema constructs
+metadata = MetaData()
+
+# create the Flask SQLAlchemy extension
+db = SQLAlchemy(metadata=metadata)
+
+# define a model class by inheriting from db.Model.
+
+
+class Employee(db.Model):
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    salary = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Employee {self.id}, {self.name}, {self.salary}>'
+if __name__=='__main__':
+    app.run()
